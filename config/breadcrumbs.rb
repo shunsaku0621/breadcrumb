5b9e0b4b93b69232486ef76546685c82bec9cf30
shunsaku0621 <hamagi.0621@icloud.com>
2021-08-19 13:11:17 +0900

    パンくずリストの作成

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -2,6 +2,16 @@ crumb :root do
   link "Home", root_path
 end
 
+
+crumb :tweets do
+  link "ツイート一覧", tweets_index_path
+  parent :root
+end
+
+crumb :contacts do
+  link "コンタクト", contacts_index_path
+  parent :tweets
+end
 # crumb :projects do
 #   link "Projects", projects_path
 # end
